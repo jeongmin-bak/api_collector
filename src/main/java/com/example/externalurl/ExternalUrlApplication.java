7524d7d5f5c52c39be4928dfb41aab93eca81a78
jeongmin-bak <pjm9673@gmail.com>
2025-08-01 16:13:55 +0900

    main 함수 완료

--- src/main/java/com/example/externalurl/ExternalUrlApplication.java
+++ src/main/java/com/example/externalurl/ExternalUrlApplication.java
@@ -41,6 +41,62 @@ public class ExternalUrlApplication implements CommandLineRunner{
             log.error("작업 메타 정보를 읽어 오는데 실패 했습니다 : {}", e.getMessage());
             throw new RuntimeException(e);
         }
+
+        // 필수 요소 파라미터
+        int contCnt = (int) metaInfo.get("CON_CNT");
+        executorService = Executors.newFixedThreadPool(contCnt);
+
+        boolean isQuery = Boolean.parseBoolean((String) metaInfo.get("IS_QUERY"));
+        boolean isPath = Boolean.parseBoolean((String) metaInfo.get("IS_PATH"));
+        List<Map<String, Object>> apiParam = (List<Map<String, Object>>) metaInfo.get("API_PARAM");
+
+        log.info("apiParam 요청 갯수 : {}", apiParam.size());
+
+        UrlHandler urlHandler;
+        if (isPath) {
+            urlHandler = context.getBean(PathUrlHandler.class);
+        } else if(isQuery) {
+            urlHandler = context.getBean(QueryUrlHandler.class);
+        } else {
+            throw new RuntimeException("제공되지 않는 URL 형식입니다.");
+        }
+
+        int BATCH_SIZE = apiParam.size();
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        for(int i = 0 ; i < BATCH_SIZE; i++) {
+            final int index = i;
+            if (i != 0 && i % 500 == 0) {
+                try {
+                    log.info("500번이상 요청되어 5초간 대기 후 요청진행합니다.");
+                    Thread.sleep(5000);
+                } catch(InterruptedException e) {
+                    log.error(e.getMessage());
+                }
+            } else {
+                futures.add(CompletableFuture.runAsync(() -> urlHandler.handle(apiParam.get(index), metaInfo), executorService));
+            }
+        }
+
+        //List<CompletableFuture<Void>> futures = new ArrayList<>();
+        //apiParam.forEach(urlParam -> {
+        //    futures.add(CompletableFuture.runAsync(() -> urlHandler.handle(urlParam, metaInfo), executorService));
+        //});
+
+        CompletableFuture<Void> allDoneFuture =
+                CompletableFuture.allOf(futures.toArrray(new CompletableFuture[0]));
+        allDoneFuture
+            .thenRun(() -> {
+                log.info("모든 작업 성공");
+                sendStatus.sendCompleteStatusUpdate(jbId);
+            })  .exceptionally(ex -> {
+                            log.error("에러 발생 : {}", ex.getMessage(), ex);
+                            sendStatus.sendStatusUpdate(jbId, "99", ex.getMessage());
+                            return null;
+                        });
+        allDoneFuture.join(); //동기적으로 기다리기
+        log.info("외부데이터 수집 완료");
     }
+
+    
 
 }
