6e1c16b62eaa0b9335d6bca3ee0c7ee09e4932c7
jeongmin-bak <pjm9673@gmail.com>
2025-08-01 15:58:24 +0900

    외부데이터 main 반영

--- src/main/java/com/example/externalurl/ExternalUrlApplication.java
+++ src/main/java/com/example/externalurl/ExternalUrlApplication.java
@@ -4,10 +4,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @SpringBootApplication
-public class ExternalUrlApplication {
+@RequiredArgsConstructor
+public class ExternalUrlApplication implements CommandLineRunner{
+
+    private final ObjectMaper objectMapper;
+    private final ApplicationContext context;
+    private final SendStatus sendStatus;
+
+    @Value("${common.params.meta_info_path}")
+    private String jsonFilePath;
+
+    private ExecutorService executorService;
+
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(ExternalUrlApplication.class)
+                            .web(WebApplicationType.NONE)
+                            .run(args)
+        SpringApplication.exit(context, () -> 0);
         SpringApplication.run(ExternalUrlApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        String jbId = args[0];
+        log.info("외부 데이터 수집 시작");
+        sendStatus.sendStatusUpdate(jbId, "01", ""); // 외부데이터 작업 시작 전송 신호
+        String jobMetaFile = jsonFilePath + "/api_unload_" + jbId + ".json";
+        log.info("메타 파일 이름 : {}", jobMetaFile);
+
+        Map<String, Object> metaInfo;
+        try {
+            metaInfo = objectMapper.readValue(new File(jobMetaFile), HashMap.class);
+        } catch (IOException e) {
+            log.error("작업 메타 정보를 읽어 오는데 실패 했습니다 : {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
 }
