afc12d0dacde886f74b1d2f33d7c25356fd69872
jeongmin-bak <pjm9673@gmail.com>
2025-08-21 09:38:53 +0900

    req -> res

---
+++ src/main/java/com/example/externalurl/util/FetchApi.java
@@ -0,0 +1,55 @@
+@Slf4j
+public class FetchApi {
+    public static String fetchApiData(String apiUrl, String dataFormat) {
+        int MAX_RETRIES = 5;
+        ObjectMapper objectMapper = new ObjectMapper();
+        String response = "";
+        boolean successCheck = false;
+
+        try {
+            URL url = new URL(apiUrl);
+            BufferedReader br;
+            for (int attempt = 1; attempt <= MAX_RETRIES; atempt++) {
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                try {
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("Content-type", String.format("application/%s". dataFormat));
+                    conn.setConnectTimeout(600000);
+                    conn.setReadTimeout(600000);
+
+                    String resContentType = conn.getHeaderField("Content-Type").toLowerCase();
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK || (conn.getResponseCode >= 200 && conn.getResponseCode() <= 300)) {
+                        br = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF-8));
+                        successCheck = true;
+                        break;
+                    }
+                    if (conn.getResponseCode == 504) {
+                        br = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF-8));
+                        response = br.lines().collect(Collectors.joining());
+                        successCheck = false;
+                        log.error("응답값 에러 발생!! {}", response);
+
+                        if (response.contains("fallback")) {
+                            Map<String, String> tmpMap = objectMapper.readValue(response, Map.class);
+                            if (tmpMap.get("message")).contains(("fallback")) {
+                                log.info("재시도 요청 : {} fallback 문제로 다시 수집 요청", attempt);
+                                response = "";
+                                continue;
+                            }
+                        } else if(!resContentType.contains(dataFormat.toLowerCase())) {
+                            log.info("응답값이 사용자 요청과 다른형식으로 응답되었습니다.");
+                            response = "";
+                        }
+                        break;
+                    }       
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    if(conn != null) {
+                        conn.disconnect();
+                    }
+                }
+            }
+        }
+    }
+}
